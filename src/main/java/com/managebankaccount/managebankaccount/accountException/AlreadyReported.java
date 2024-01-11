819cedb778a2d32a3ef78c8bc3ed0a2fa05ca371
LuanBui02 <154111094+LuanBui02@users.noreply.github.com>
2024-01-11 10:25:18 +0700

    Delete src/main/java/com/managebankaccount/managebankaccount/accountException/AlreadyReported.java

--- src/main/java/com/managebankaccount/managebankaccount/accountException/AlreadyReported.java
+++
@@ -1,7 +0,0 @@
-package com.managebankaccount.managebankaccount.accountException;
-
-public class AlreadyReported extends RuntimeException{
-    public AlreadyReported(long id) {
-        super(String.format("This id %d is already used", id));
-    }
-}
