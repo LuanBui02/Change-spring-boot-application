077f56895884b1dda75fbe6e1789a32dc5cb7a41
LuanBui02 <154111094+LuanBui02@users.noreply.github.com>
2024-01-11 09:55:11 +0700

    Delete src/main/java/com/managebankaccount/managebankaccount/details/advice directory

--- src/main/java/com/managebankaccount/managebankaccount/details/advice/AlreadyReported.java
+++
@@ -1,7 +0,0 @@
-package com.managebankaccount.managebankaccount.details.advice;
-
-public class AlreadyReported extends RuntimeException{
-    public AlreadyReported(long id) {
-        super(String.format("This id %d is already used", id));
-    }
-}
--- src/main/java/com/managebankaccount/managebankaccount/details/advice/NoSpaceInPassword.java
+++
@@ -1,7 +0,0 @@
-package com.managebankaccount.managebankaccount.details.advice;
-
-public class NoSpaceInPassword extends RuntimeException{
-    public NoSpaceInPassword() {
-        super("Password can not have space");
-    }
-}
