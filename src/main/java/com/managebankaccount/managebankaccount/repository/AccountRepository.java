29d5715fe677d849ccf6854ef0bbb0bf7c4c4cba
LuanBui02 <154111094+LuanBui02@users.noreply.github.com>
2024-01-11 10:12:30 +0700

    Delete src directory

--- src/main/java/com/managebankaccount/managebankaccount/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.managebankaccount.managebankaccount.repository;
-
-import com.managebankaccount.managebankaccount.beans.AccountUsers;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccountRepository extends JpaRepository<AccountUsers, Long> {
-}
