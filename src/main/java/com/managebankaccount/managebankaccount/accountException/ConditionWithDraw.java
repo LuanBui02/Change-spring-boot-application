d9d4f38000c5f7c6b4329e47bce06c0a5bc0827c
LuanBui02 <154111094+LuanBui02@users.noreply.github.com>
2024-01-11 10:25:33 +0700

    Delete src/main/java/com/managebankaccount/managebankaccount/accountException/ConditionWithDraw.java

--- src/main/java/com/managebankaccount/managebankaccount/accountException/ConditionWithDraw.java
+++
@@ -1,9 +0,0 @@
-package com.managebankaccount.managebankaccount.accountException;
-
-import com.managebankaccount.managebankaccount.utils.Constant;
-
-public class ConditionWithDraw extends RuntimeException{
-    public ConditionWithDraw() {
-        super(Constant.conditionWithDraw);
-    }
-}
