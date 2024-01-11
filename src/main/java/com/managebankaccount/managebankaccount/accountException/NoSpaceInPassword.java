65d00d12df9e824fcdf630e8452ac627d09e6c05
LuanBui02 <154111094+LuanBui02@users.noreply.github.com>
2024-01-11 10:25:44 +0700

    Delete src/main/java/com/managebankaccount/managebankaccount/accountException/NoSpaceInPassword.java

--- src/main/java/com/managebankaccount/managebankaccount/accountException/NoSpaceInPassword.java
+++
@@ -1,7 +0,0 @@
-package com.managebankaccount.managebankaccount.accountException;
-
-public class NoSpaceInPassword extends RuntimeException{
-    public NoSpaceInPassword() {
-        super("Password can not have space");
-    }
-}
